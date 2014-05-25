b5dc4de95834f95102f10607844afa7f6ee5d8f4
Sebastián Puente <sebas7pr@gmail.com>
2014-05-25 14:09:36 -0500

    Add 'about' page test and implementation

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,17 @@ describe "Static pages" do
   
   describe "Help page" do
 
-      it "should have the content 'Help'" do
-        visit '/static_pages/help'
-        expect(page).to have_content('Help')
-      end
-    end  
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end  
+    
+  describe "About page" do
+    
+    it "should have the content 'About us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
